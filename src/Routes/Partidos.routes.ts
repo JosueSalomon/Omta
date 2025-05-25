bdbbe4aea90a3daca5429cc3f56633facd16ba48
StephanyNM4 <stephanymatamoros4@gmail.com>
2025-05-25 00:45:05 -0600

    finalizar partido

--- src/Routes/Partidos.routes.ts
+++ src/Routes/Partidos.routes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { cancelarPartido, finalizarPartido } from '../Controllers/Partido.controller'
 
 const router = express.Router();
-
+//rutas
 router.get('/cancelar/:id', cancelarPartido);
 router.get('/finalizar/:id', finalizarPartido);
 
