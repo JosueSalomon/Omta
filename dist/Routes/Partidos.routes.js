5d1477425a9e6adae5a66945bdb79e01efd07f1d
StephanyNM4 <stephanymatamoros4@gmail.com>
2025-05-25 00:43:16 -0600

    finalizar partido

--- dist/Routes/Partidos.routes.js
+++ dist/Routes/Partidos.routes.js
@@ -7,4 +7,5 @@ const express_1 = __importDefault(require("express"));
 const Partido_controller_1 = require("../Controllers/Partido.controller");
 const router = express_1.default.Router();
 router.get('/cancelar/:id', Partido_controller_1.cancelarPartido);
+router.get('/finalizar/:id', Partido_controller_1.finalizarPartido);
 exports.default = router;
--- src/Routes/Partidos.routes.ts
+++ src/Routes/Partidos.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { cancelarPartido } from '../Controllers/Partido.controller'
+import { cancelarPartido, finalizarPartido } from '../Controllers/Partido.controller'
 
 const router = express.Router();
 
-router.get('/cancelar/:id', cancelarPartido)
+router.get('/cancelar/:id', cancelarPartido);
+router.get('/finalizar/:id', finalizarPartido);
 
 export default router;
