3bad16f248e8d67ca97c5744e57dc925cf2d1427
StephanyNM4 <stephanymatamoros4@gmail.com>
2025-05-25 17:35:27 -0600

    json web token

--- src/Models/Administrator.model.ts
+++ src/Models/Administrator.model.ts
@@ -14,4 +14,28 @@ export class Administrator {
 
     return data;
     }
+
+static async insertUserToken(user: any, contrasenia: any, token: any): Promise<any> {
+    try {
+        const { data, error } = await supabase.rpc('insertar_token_usuario', {
+        p_user: user,
+        p_contrasenia: contrasenia,
+        p_token: token
+        });
+
+        if (error) {
+            console.log(error)
+        throw new Error(`Error: ${error.message}`)
+        }
+        
+        console.log(data);
+
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+        throw new Error(error.message);
+        } else {
+        throw new Error("Unknown error");
+        }
+    }
+    }
 }
